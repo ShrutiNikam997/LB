e98891ed214f01abf24e94513cf1f53fbf46f382
ShrutiNikam997 <shrutinikam997@gmail.com>
2024-11-26 18:35:34 +0530

    Add files via upload

---
+++ program12.c
@@ -0,0 +1,36 @@
+//student is pass or fail 
+
+#include<stdio.h>
+#include<stdbool.h>
+
+bool DisplayResult(float fMarks)
+{
+    if((fMarks >= 0.0f) && (fMarks < 35.0f))
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+int main()
+{
+    float fValue = 0.0f;
+    bool bRet = false;
+
+    printf("Please enter your percentage : ");
+    scanf("%f",&fValue);
+
+    bRet = DisplayResult(fValue);//function call
+
+    if(bRet == true)
+    {
+        printf("Student is pass in the exam\n");
+    }
+    else
+    {
+        printf("Student is fail in the exam\n");
+    }
+    return 0;
+}
---
+++ program23.c
@@ -0,0 +1,21 @@
+/**********************************whileLoop********************************/
+#include<stdio.h>
+
+void Display()
+{
+    int iCnt = 0;
+
+    iCnt = 1;                       //1
+    while( iCnt <= 3 )               //2 
+    {
+        printf("Jay Hanuman...\n"); //4     
+        iCnt++;                     //3
+    }
+}
+
+int main()
+{
+    Display();         
+
+    return 0;
+}
